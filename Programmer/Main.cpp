cb2577dc7ea37e1729fa803395010c9cb93ce375
Yaneww11 <93374652+Yaneww11@users.noreply.github.com>
2023-04-02 22:07:41 +0300

    Update Main.cpp

--- Programmer/Main.cpp
+++ Programmer/Main.cpp
@@ -5,7 +5,16 @@ using namespace std;
 
 int main()
 {
-    Employee e ();
-  
+    Employee e;
+    Programmer p("Ivan", 1689);
+    cout << e << endl;
+    cout << p << endl;
+    e.set_name("Petyr");
+    cout << e.get_name() << endl;
+    cout << p.get_name() << endl;
+    e = p;
+    cout << e;
+
+
     return 0;
 }

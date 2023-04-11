80b92ad2aa6a15e902c19a1e651741cdc767bed6
Yaneww11 <93374652+Yaneww11@users.noreply.github.com>
2023-04-11 17:45:14 +0300

    Update and rename Car to Car.h

---
+++ CarProject/Car.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+class Car;
+Car operator+(const Car&, const int&);
+Car operator-(const Car&, const int&);
+ostream& operator<<(ostream& out, const Car& c);
+
+class Car
+{
+    friend Car operator+(const Car&, const int&);
+    friend Car operator-(const Car&, const int&);
+    friend ostream& operator<<(ostream& out, const Car& c);
+private:
+    string make;
+    string model;
+    string color;
+    int top_speed;
+
+public:
+    Car();
+    Car(string , string , string , int );
+
+    string getMake();
+    string getModel();
+    string getColor();
+    int getTopSpeed();
+
+    void set_top_speed(int);
+
+    Car& operator=(const Car&);
+
+    friend class Person;
+};
